5863266eaf1e80b3fe4c6e5ce60377bca1336b71
Derrickmaina18 <derrickmaina18@gmail.com>
2022-06-07 16:26:30 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ * @r: print int r
+ * @n: print int n
+ * Return: Always 0.
+ */
+
+int add(int r, int n)
+
+{
+	return (r + n);
+}
