94a698eb683f36d3db003c5951a6153f0a857e3a
Derrickmaina18 <derrickmaina18@gmail.com>
2022-06-07 15:57:15 -0700

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit() {
+	int r, digit;
+	digit = r % 10;
+	_putchar(digit);
+	_putchar("/n");
+	return 0;
+}
