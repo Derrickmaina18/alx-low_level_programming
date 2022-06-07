56c4f4af5ff066f1f45079dbad93bf467a4c48eb
Derrickmaina18 <derrickmaina18@gmail.com>
2022-06-07 16:12:11 -0700

    last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,26 @@
-#include "stdio.h"
-
+#include "holberton.h"
 /**
- * print_last_digit - prints the last digit of a number
- * @n: n is an integer
- * Description: prints the last digit of a number
- * Return: integer
+ * _abs- returns 1 if n is lowercase, 0 otherwise
+ * @n: integer to be tested
+ * Return: abs(n)
  */
-
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
+/**
+ * print_last_digit - pritns the last digit of a number
+ * @n: integer whose last digit is to be printed
+ * Return: lastdigit of n
+ */
 int print_last_digit(int n)
 {
-	int last = n % 10;
+	int lastDigit;
 
-	if (n < 0)
-	{
-		last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+	lastDigit = _abs((n % 10));
+	_putchar(lastDigit + 48);
+	return (lastDigit);
 }
