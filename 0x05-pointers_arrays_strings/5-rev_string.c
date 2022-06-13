02901f048a3cebd59f40a054a2ee96ac74d489a7
Derrickmaina18 <derrickmaina18@gmail.com>
2022-06-13 13:50:15 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,33 @@
-#include "main.h"
-/*
-*rev_string: reverse the elements of an array
-*@*s: pointer of type array of string
-Return: void
-*/
+#include "holberton.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
 void rev_string(char *s)
 {
-int i = 0, j = 0;
-char tmp[20];
-while (*(s + i))
-    {
-    *(tmp + i) = *(s + i);
-    i++;
-    }
-i = i - 1;
-while (i >= 0)
-    {
-    *(s + i) = *(tmp + j);
-    j++;
-    i--;
-    }
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
 }
